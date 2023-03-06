5c284f50291daa8d1e7f689c170952ba38816dc7
Mansur <vitrigovm@gmail.com>
2023-03-06 16:08:54 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import TodoList from './features/todos/todosList.jsx';
+
+function App() {
+    return <TodoList />;
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+);
